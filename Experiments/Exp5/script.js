0253ea76e8643027c6296d0e658cef5828427b7a
dishantgahlot <dishantghalot9@gmail.com>
2026-04-23 09:41:07 +0530

    ch

--- Experiments/Exp5/script.js
+++ Experiments/Exp5/script.js
@@ -69,7 +69,7 @@ console.log(total);
 let students= [
     {name:"Alice",marks:85},
     {name:"Bob",marks:92},
-    {name:"Charlie",marks:78}
+    {name:"Charlie",marks:78},
 ];
 
 let marks = students.map(student =>student.marks);
